97f19b190634385a2b31bede38d2c84b20566864
berahm25 <amirjan2097@gmail.com>
2025-09-01 23:53:36 +0430

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Mv80VaDS#C1nMNGvoC6Y2guryB4i7-eIJmVurW0aCG2w3rza-o5I",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "93744734595", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
